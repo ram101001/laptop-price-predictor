c41b347bebf9b2b64b18f26cb6e65ee7fa882d6f
ram101001 <105034134+ram101001@users.noreply.github.com>
2023-07-05 00:52:47 +0530

    Created simple web app for laptop price prediction
    Created a simple and working Laptop price predictor model which has a r2 score of 0.89 using xgboost.
    
    The model uses a very recent dataset so it can be used by anyone for getting an idea and selecting a good laptop according to their required configurations in budget.

---
+++ app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load the model
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+
+st.title("Laptop Predictor")
+
+# Load the dataframe
+df = pickle.load(open('df.pkl', 'rb'))
+
+# Brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# Type of laptop
+type = st.selectbox('Type', df['TypeName'].unique())
+
+# RAM
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# Weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# Screen size
+screen_size = st.number_input('Screen Size')
+
+# Resolution
+resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
+
+# OS
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5/screen_size
+    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os],dtype=object)
+
+    query = query.reshape(1, 12)
+    predicted_price = pipe.predict(query)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(predicted_price[0]))))
